7e8305d5de28c2bc5154464f868104b1b7ed674b
kavyanjali31 <81075366+kavyanjali31@users.noreply.github.com>
2022-02-13 16:52:21 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,41 @@
+class Game{
+    constructor(){
+    }
+    getState(){
+        var gameStateRef=database.ref('gameState')
+        gameStateRef.on('value',function(data){
+            gameState=data.val()
+        })
+    }
+    update(state){
+    database.ref('/').update({
+        gameState:state
+    })
+
+    
+    }
+    start(){
+    form=new Form()
+    form.display()
+    player=new Player()
+    playerCount=player.getCount()
+    bike1=createSprite(300,300)
+    bike1.addImage("bike1",bike1_img)
+    bike2=createSprite(100,100)
+    bike2.addImage("bike2",bike2_img)
+    bikes=[bike1,bike2]
+    }
+    handleElements(){
+    form.hide();
+    form.titleImg.position(40,50)
+    }
+    play(){
+        this.handleElements()
+        Player.getPlayersInfo()
+        if(allPlayers!==undefined){
+            image(track,0,-height*5,width,height*6)
+            drawSprites()
+            
+        }
+    }
+}
